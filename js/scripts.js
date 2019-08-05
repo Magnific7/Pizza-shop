c89655b6099b36b6caf5cb53a794dc6043ab1244
Rithaven <veneruth4@gmail.com>
2019-08-05 11:20:58 +0200

    these

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,3 @@ function Order(size,crust,toppings,number,delivery){
   Order.prototype.fullInfo=function(){
      return this.pizzaSize + "," + this.pizzaCrust + "," + this.pizzatoppings + "," + this.pizzaNumber + "," + this.pizzaDelivery;
   }
-
-
-
-  // user interface Logic
-$(document).ready(function(){
-  $('button').click(function(){
-  // event.preventDefault();
-  console.log('error')
-  if($('#pizza-size1:checked').val()){
-     var inputtedPizzaSize=$('#pizza-size1').val();
-     console.log(inputtedPizzaSize)
-    }
